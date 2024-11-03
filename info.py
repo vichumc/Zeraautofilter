5d11fa349394172da14362bf335da546d1e8549d
Vishnu V <100103080+vichumc@users.noreply.github.com>
2024-11-03 21:51:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,10 +71,10 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/vj_bot_disscussion')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vj_bots')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+7XRfdaOtii82ODE1')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+yB5VoNNIt4xkY2Vl')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/+yB5VoNNIt4xkY2Vl')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 't.me/+yB5VoNNIt4xkY2Vl') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
